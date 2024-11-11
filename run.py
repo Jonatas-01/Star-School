2c09a174cfeb4a1786d4f99b5cd790b6b6a67640
Jonatas-01 <jonatasmc02@gmail.com>
2024-11-11 11:47:39 +0000

    feat:Create welcome_page function to tells user the application is running

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,4 +16,19 @@ SHEET = GSPREAD_CLIENT.open('star_school')
 students = SHEET.worksheet('students')
 data = students.get_all_values()
 
-print(data)
+def welcome_page():
+    """
+     Display the Welcome Page to customer
+    """
+    print(r"""
+         ______   ________   ________   ________      ______   __   __     _____   ________   ________   __
+        /   ___) |__    __| |   __   | |   __   |    /   ___) |  | |  |   /   __| |   __   | |   __   | |  |
+       (   (__      |  |    |  |__|  | |  |__| _/   (   (__   |  |_|  |  /   /    |  |  |  | |  |  |  | |  |
+        \__   \     |  |    |   __   | |   __   \    \__   \  |   _   | (   (     |  |  |  | |  |  |  | |  |
+        ___)   )    |  |    |  |  |  | |  |  |  |    ___)   ) |  | |  |  \   \__  |  |__|  | |  |__|  | |  |_____
+       (______/     |__|    |__|  |__| |__|  |__|   (______/  |__| |__|   \_____| |________| |________| |________|
+    """)
+    print("Welcome to Star School!")
+    print("Here you can have access to manage all students data.")
+
+welcome_page()
